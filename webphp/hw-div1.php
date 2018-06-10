ea545c0b250f93da79330e3840b40b6f912fb249
joyce001121 <39973753+joyce001121@users.noreply.github.com>
2018-06-10 22:40:20 +0800

    Add files via upload

---
+++ webphp/hw-div1.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>無標題文件</title>
+</head>
+
+<body style="padding: 0px; margin: 0px;">
+<div align="center" id="divBox">
+     
+   <div id="divHeader" style="padding: 0px; margin: 0px; height: 500px; width: 100%; background-repeat: no-repeat; background-size: 100%; background-image: url(images/header_background.jpg);"> <br />
+
+   		<div id="divTitle" style="top: 0px; text-align: center; width: 870px; height: 85px;">
+   		  <div id="divLogo" style="width: 145px; height: 85px; float: left; padding: 0px; margin: 0px; background-size: 80%; background-repeat: no-repeat; background-image: url(images/logo.png); "> </div>
+          <div id="divItem1" style="color: #FFF; line-height: 140px; width: 145px; height: 85px; vertical-align: bottom; text-align: center; float: left;">關於輝瑞 </div>
+          <div id="divItem2" style="color: #FFF;line-height: 140px; width: 145px; height: 85px; vertical-align: bottom; text-align: center; float: left;">企業訊息 </div>
+          <div id="divItem3" style="color: #FFF;line-height: 140px; width: 145px; height: 85px; vertical-align: bottom; text-align: center; float: left;">新藥研發 </div>	
+          <div id="divItem4" style="color: #FFF;line-height: 140px; width: 145px; height: 85px; vertical-align: bottom; text-align: center; float: left;">產品資訊 </div>
+                  <div id="divItem5" style="width: 145px; height: 85px; vertical-align: bottom; text-align: center; float: left;">
+                  		   <div id="search" style="color: #FFF;width: 100%; height: 50%;"> Search</div>
+                  		   <div id="health" style="color: #FFF;line-height: 55px; width: 100%; height: 50%;" > 健康小百科 </div>
+                  </div>
+                  
+        </div>
+        <div id="divTitle2" style="top: 0px; text-align: center; width: 870px; height: 396px;"> 
+        		<div id="title21" style="width:70%; height: 100%; float:left;"> </div>
+                <div id="title22" style="  width: 30%; height: 100%; float: left;"> 
+                	<div id="divMain" style="  width: 100%; height: 30%; "> </div>
+                    <div id="divMain" style="color: #FFF;background-color: #0061bc; width: 100%; height: 70%;">我們的目標 </div>
+                </div>
+ 
+        </div>
+        
+   </div>  
+   
+   </div>
+   
+   
+   <div align="center" id="divMain" style="background-color: #FFC;  width: 100%;"> 
+   		<div id="divMain11" style=" background-position: center center; background-color: #CCC; width: 940px;">
+             <div id="divMain11" style="width: 620px; height: 400px; float: left; background-color: #99F;">aaa </div>
+             <div id="divMain12" style="width: 300px; height: 400px; float: left; background-color: #9FC;">bbb </div>
+     </div>
+        <div id="divMain21" style=" background-position: center center; background-color: #CCC; width: 940px;">
+             <div id="divMain11" style="width: 33%; height: 400px; float: left; background-color: #F69;">aaa </div>
+             <div id="divMain12" style="width: 33%; height: 400px; float: left; background-color: #F9F;">bbb </div>
+             <div id="divMain12" style="width: 33%; height: 400px; float: left; background-color: #FCF;">bbb </div>
+     </div>
+   </div>
+   
+
+ 
+   
+   
+   
+   
+  <p>&nbsp;</p>
+</div> 
+</body>
+</html>
---
+++ webphp/hw-div2.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>ESL Podcast</title>
+</head>
+
+<body style="padding: 0px; margin: 0px;">
+<!-- https://itunes.apple.com/us/podcast/english-as-second-language-esl-podcast-learn-english/id75908431?mt=2 -->
+
+<div align="center" id="divBox">
+
+<div id="divHeader" style="width: 100%; height: 44px; background-color: #333;" >
+ 
+ 	<div align="center" id="divMenu" style="width: 1000px; height: 44px; ">
+ 		<div id="divLogo" ><img src="https://www.apple.com/ac/globalnav/3/en_US/images/globalnav/apple/image_large.svg" style="float: left; width: 40px; height: 44px;" /> </div>
+        <div id="divMenu" style="float: left; width: 960px; height: 44px; background-color: #CCC;" > 
+       	  <div id="divMenu1" style="line-height: 55px;   width: 11.11%; height: 44px; float: left;"  > aaa</div>
+          <div id="divMenu1" style="line-height: 55px; width: 11.11%; height: 44px; float: left;"  > aaa</div>
+          <div id="divMenu1" style="line-height: 55px; width: 11%; height: 44px; float: left;"  > aaa</div>
+          <div id="divMenu1" style="line-height: 55px;  width: 11%; height: 44px; float:left; "  > aaa</div>
+          <div id="divMenu1" style="line-height: 55px;  width: 11%; height: 44px; float:left; "  > aaa</div>
+          <div id="divMenu1" style="line-height: 55px; width: 11%; height: 44px; float:left; "  > aaa</div>
+          <div id="divMenu1" style="line-height: 55px;  width: 11%; height: 44px; float:left; "  > aaa</div>
+          <div id="divMenu1" style="line-height: 55px;  width: 11%; height: 44px; float:left; "  > aaa</div>
+          <div id="divMenu1" style="line-height: 55px;  width: 11%; height: 44px; float:left; "  > aaa</div>
+        </div>
+        
+    </div> 
+        
+ </div>
+ <br>
+ <div id="divMain1" style="width: 1000px; height: 32px; background-color: #9FF;" > 
+ 	  <div id="divMain11" style="vertical-align: bottom; float: left; width: 270px; height: 32px; float: left;"> <img src="images/itunespreview.png" width="95%" /> </div>
+      <div id="divMain12" style="float: left; width: 400px; height: 32px;float:left; "> </div>
+      <div id="divMain13" style="background-color: #9CF; float: left; width: 330px; height: 32px; float: left;"> </div>
+ </div>
+ 
+ <div id="divMain2" style="width: 1000px; height: 210px; background-color: #FFC;" > 
+   <div id="divMain21" style="position: relative; width: 170px; height: 210px; background-color: #FFC; float: left;" ><br><img src="images/callout_screenshot.png" width="100%" /> </div>
+   <div id="divMain22" style="width: 660px; height: 210px; background-color: #FFC;float:left;" >  </div>
+   <div id="divMain23" style="width: 170px; height: 210px; background-color: #FFC;float:left;" >  </div>
+ </div>  
+ <br> 
+ 
+ <div id="divMain31" style="width: 1000px; height: 100px; background-color: #CCC;" >  </div>
+
+ <div id="divMain4" style="width: 1000px; height: 500px; background-color: #FFC;" >  
+	<div id="divMain41" style="height: 500px; float: left; width: 25%; background-color: #FCF;" > </div>
+    <div id="divMain42" style="height: 500px; float: left; width: 75%; background-color: #CCF;" > </div>
+ </div>
+ 
+ <div id="divFooter" > </div>
+ 
+</div>
+
+https://www.apple.com/ac/globalnav/3/en_US/images/globalnav/apple/image_large.svg
+</body>
+</html>
